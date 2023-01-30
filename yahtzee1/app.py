6cde8c6a2be09363ad9ed42d3b1af6c13ebae362
Markus Lindgren <mavn22@bth.se>
2023-01-30 02:35:23 +0100

    implemented hand.py and die.py

--- yahtzee1/app.py
+++ yahtzee1/app.py
@@ -4,8 +4,8 @@ Yahtzee app, html test module
 """
 # Importera relevanta moduler
 from flask import Flask, render_template
-import src.die as die
-import src.hand as hand
+from src.die import Die
+from src.hand import Hand
 import random
 
 app = Flask(__name__)
@@ -19,10 +19,12 @@ def index():
 def main():
     """ Main route """
     # Create a hand.
-    gameHand = hand.Hand()
+    gameHand = Hand()
+    # Rolls 5 dice
+    gameHand.roll(5)
 
-    # Returns every die rolled once.
-    return render_template("main.html", dice1 = gameHand.roll(1), dice2 = gameHand.roll(2), dice3 = gameHand.roll(3), dice4 = gameHand.roll(4), dice5 = gameHand.roll(5))
+    # Returns value of every die in hand.
+    return render_template("main.html", dice1 = gameHand.dice[0].get_value(), dice2 = gameHand.dice[1].get_value(), dice3 = gameHand.dice[2].get_value(), dice4 = gameHand.dice[3].get_value(), dice5 = gameHand.dice[4].get_value())
 
 @app.route("/about")
 def about():

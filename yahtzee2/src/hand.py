23ede67487ee61c7106e1f469fa6e6245df9f134
Markus Lindgren <mavn22@bth.se>
2023-02-17 07:34:13 +0100

    to_list metod tillagd

--- yahtzee2/src/hand.py
+++ yahtzee2/src/hand.py
@@ -26,3 +26,12 @@ class Hand:
         retunera namn på tärningar med "," separerad
         """
         return ", ".join(str(die) for die in self.dice)
+
+    def to_list(self):
+    """
+    Returnerar en lista som innehåller värdet av varje tärning i handen.
+    """
+    value_list = []
+    for die in self.dice:
+        value_list.append(die.get_value())
+    return value_list

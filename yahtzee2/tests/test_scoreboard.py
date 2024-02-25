97a11334cd8f76f745d8cbe1ce6d2450e0dc89d2
Markus Lindgren <mavn22@bth.se>
2024-02-25 14:06:06 +0100

    first commit

---
+++ yahtzee2/tests/test_scoreboard.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+#pylint: disable=protected-access
+""" Module for testing the class Scoreboard """
+import unittest
+import random
+from src.scoreboard import Scoreboard
+
+class TestScoreboard(unittest.TestCase):
+    """ Submodule for unittests, derives from unittest.TestCase """
+    ...

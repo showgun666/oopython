8206acbf37c654376635654fb879175b9311da8a
Markus Lindgren <mavn22@bth.se>
2024-02-25 13:33:47 +0100

    test for rolling hand without argument

--- yahtzee2/tests/test_hand.py
+++ yahtzee2/tests/test_hand.py
@@ -56,4 +56,13 @@ class TestDie(unittest.TestCase):
         self.assertFalse(my_hand.dice[4] == 1)
 
     def test_roll_without_argument(self):
-        ...
+        """ Attempt to roll a hand without an argument """
+        my_hand = Hand([5,1,2,3,1,6])
+
+        my_hand.roll()
+
+        self.assertFalse(my_hand.dice[0] != 5)
+        self.assertFalse(my_hand.dice[1] != 1)
+        self.assertFalse(my_hand.dice[2] != 2)
+        self.assertFalse(my_hand.dice[3] != 3)
+        self.assertFalse(my_hand.dice[4] != 1)

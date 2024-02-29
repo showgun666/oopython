990f55d66ab407145309b3973f766e069b7e2222
Markus Lindgren <mavn22@bth.se>
2024-02-29 17:23:46 +0100

    Append method added

--- yahtzee2/src/unorderedList.py
+++ yahtzee2/src/unorderedList.py
@@ -2,3 +2,45 @@
 Denna modul innehåller en klass som simulerar en unordered list
 Detta är för att lära mig datastrukturer.
 """
+from src.node import Node
+
+class UnorderedList():
+    """
+    Unordered List class
+    """
+    def __init__(self):
+        """
+        Initiate a new unordered list instance
+        """
+        self.head = Node(1)
+
+    def append(self, data):
+        """
+        Append data to the end of the unordered list.
+        """
+        current_node = self.head
+
+        # Point current_node to the next node in the list until it points to None
+        while current_node.next != None:
+            current_node = current_node.next
+
+        # Point the current node to a new node with the data to be appended to the list.
+        current_node.next = Node(data)
+
+    def set(self, index, data):
+        ...
+
+    def size(self):
+        ...
+    
+    def get(self, index):
+        ...
+    
+    def index_of(self, value):
+        ...
+    
+    def print_list(self):
+        ...
+    
+    def remove(self, data):
+        ...

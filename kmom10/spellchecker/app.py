8b6f97f54caf7f6c8f5a34abd0c45ad88e382d4f
showgun666 <SE2321000016-H8HB@sll.se>
2024-08-26 12:10:04 +0200

    working on the flask side of things. UML added. Flask#3 done. Flask#4 in the works

--- kmom10/spellchecker/app.py
+++ kmom10/spellchecker/app.py
@@ -3,14 +3,45 @@
 import os
 import re
 from flask import Flask, render_template, request, redirect, url_for, session
+from src.trie import Trie
 
 app = Flask(__name__)
 app.secret_key = re.sub(r"[^a-z\d]", "", os.path.realpath(__file__))
 
-@app.route("/")
+@app.route("/", methods=["GET"])
 def main():
     "Main route"
-    return render_template("index.html")
+    dictionary_trie = Trie.create_from_file("dictionaries/tiny_dictionary.txt")
+    return render_template("index.html"
+                           )
+
+@app.route("/all-words", methods=["GET"])
+def all_words():
+    "All-words Route"
+    dictionary_trie = Trie.create_from_file("dictionaries/tiny_dictionary.txt")
+    return render_template("all-words.html",
+                           word_count = dictionary_trie.count_words(),
+                           words_list = dictionary_trie.get_words()
+                           )
+
+@app.route("/remove-word", methods=["POST"])
+def remove_word():
+    "remove-word Route"
+    return render_template("remove-word.html",
+                           )
+
+@app.route("/remove-word-from-trie", methods=["GET"])
+def remove_word_from_trie():
+    "remove-word-from-trie Route"
+    dictionary_trie = Trie.create_from_file("dictionaries/tiny_dictionary.txt")
+    return redirect(url_for('remove-word.html'))
+
+@app.route("/init", methods=["GET"])
+def init():
+    """ Intialize values needed in session """
+    removed_words = [] # Holds a list of items in order that have been removed to the dictionary.
+    session["removed_words"] = removed_words
+    return redirect(url_for('main'))
 
 @app.errorhandler(404)
 def page_not_found(e):
